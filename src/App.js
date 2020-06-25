336c0cd67d7240f14e477d03b101f0b046105139
anaccampos <anacarolina.decampos@gmail.com>
2020-06-24 23:40:04 -0300

    Front-end CRUD repositories

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
   async function handleAddRepository() {
-    // TODO
+    const payload = {
+      title: `Novo projeto ${Date.now()}`,
+      url: 'github/anaccampos',
+      techs: ['JavaScript', 'Python']
+    }
+
+    api.post('/repositories', payload)
+      .then(response => {
+        setRepositories([...repositories, response.data])
+      }).catch(err => {
+        console.log(err);
+      })
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`/repositories/${id}`)
+      .then(response => {
+        const repoIndex = repositories.findIndex(repo => (repo.id === id ));
+
+        if(repoIndex > -1){
+          const array = [...repositories];
+          array.splice(repoIndex, 1);
+          setRepositories(array);
+        }
+
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
+
+  useEffect(() => {
+    api.get('/repositories')
+      .then(response => {
+        setRepositories(response.data);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, []);
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
-        </li>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
